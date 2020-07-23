8d9d6adbb3fe7ad14ab6fd0950820d8bbd2b4f0b
René Zapata Bastidas <rzapata@laradio.cl>
2020-07-22 21:41:45 -0400

    Agregar archivos de proyecto.

---
+++ ComparaNumeros/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String e2;
+            int numeroa, numerob;
+            Console.WriteLine("Sistema para Felipe");
+            Console.WriteLine("Ingrese un número");
+            numeroa = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Ingrese un número");
+                e2 = Console.ReadLine();
+                if (e2 == "stop")
+                {
+                    Console.WriteLine("Usted Desea Cerrarlo");
+                    Environment.Exit(0);
+                }
+                numerob = int.Parse(e2);
+                if (numeroa < numerob)
+                    {
+                        numerob = numeroa;
+                    }            
+                Console.WriteLine("El numero más bajo es: " + numerob);
+            } while (Console.ReadLine() != "stop");
+        }
+    }
+}//wow es mas facil de lo que pense pero me explota la cabeza cuando no puedo :c 
